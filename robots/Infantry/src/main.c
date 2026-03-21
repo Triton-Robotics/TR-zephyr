cd53748503fb6b8df72a6264139328029f45f924
Edward <chl244@ucsd.edu>
2026-03-20 18:22:21 -0700

    basic blinky

--- robots/Infantry/src/main.c
+++ robots/Infantry/src/main.c
@@ -1,12 +1,37 @@
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
 #include <stdio.h>
+
+
+const struct device *gpio_devb = DEVICE_DT_GET(DT_NODELABEL(gpiob));
+const struct device *gpio_devc= DEVICE_DT_GET(DT_NODELABEL(gpioc));
 
 int main(void)
 {
-    printf("starting...\n");
+    if (!device_is_ready(gpio_devb) || !device_is_ready(gpio_devc)) {
+        printf("GPIO device not ready!\n");
+        return 0;
+    }
+
+    gpio_pin_configure(gpio_devb, 0, GPIO_OUTPUT);
+    gpio_pin_configure(gpio_devc, 1, GPIO_OUTPUT);
+    gpio_pin_configure(gpio_devc, 0, GPIO_OUTPUT);
+    int timer = 0;
 
     while(1) {
-        printf("looping\n");
-        k_usleep(10000);
+        timer++;
+        if (timer % 50 == 0) {
+            gpio_pin_toggle(gpio_devb, 0);
+        }
+
+        if (timer % 200 == 0) {
+            gpio_pin_toggle(gpio_devc, 1);
+        }
+
+        if (timer % 500 == 0) {
+            gpio_pin_toggle(gpio_devc, 0);
+        }
+
+        k_usleep(1000);
     }
 }
